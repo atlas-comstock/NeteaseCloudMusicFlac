f8542205aa486452ee0c6e032906b93aa73bd0c8
YongHao Hu <christopherwuy@gmail.com>
2016-04-28 23:50:05 +0800

    Demo version finished.

--- main.py
+++ main.py
@@ -1,66 +1,71 @@
 import re
 import requests
-#import json
-#import urllib2
-#import os
-url = 'http://music.163.com/playlist?id=39123016'
+import json
+import urllib2
+import os, sys
+
+print "fetching msg from "+sys.argv[1]+"\n Please make sure the url is right.\n"
+url = sys.argv[1]
+#url = 'http://music.163.com/playlist?id=39123016'
 r = requests.get(url)
 contents = r.text
 #print contents
 #print 'getcontent'
 res = r'<ul class="f-hide">(.*?)</ul>'
 mm =  re.findall(res, contents, re.S|re.M)
-#print mm[0]
+#print mm
 #print 'getcontent'
-contents = mm[0]
+if(mm):
+    contents = mm[0]
+else:
+    os._exit()
+
 res = r'<li><a .*?>(.*?)</a></li>'
 mm =  re.findall(res, contents, re.S|re.M)
+i = 0
 for value in mm:
+    if i > 10:
+        break
+    i = i+1
+    url = 'http://sug.music.baidu.com/info/suggestion'
+    payload = {'word': value, 'version': '2', 'from': '0'}
     print value
 
-#url = 'http://sug.music.baidu.com/info/suggestion'
-#payload = {'word': 'wo', 'version': '2', 'from': '0'}
-#
-#r = requests.get(url, params=payload)
-#contents = r.text
-#d = json.loads(contents, encoding="utf-8")
-#songid = d["data"]["song"][0]["songid"]
-#print songid
-#
-#url = "http://music.baidu.com/data/music/fmlink"
-#payload = {'songIds': songid, 'type': 'flac'}
-#r = requests.get(url, params=payload)
-##print r.text
-#contents = r.text
-#d = json.loads(contents, encoding="utf-8")
-##print d
-##print json_str
-#songlink = d["data"]["songList"][0]["songLink"]
-#print songlink
-#
-#songdir = "songs_dir"
-#if not os.path.exists(songdir):
-#    os.makedirs(songdir)
-#
-#songname = d["data"]["songList"][0]["songName"]
-#artistName = d["data"]["songList"][0]["artistName"]
-#filename = "./" + songdir + "/"+songname+"-"+artistName+".flac"
-#print filename
-#
-#
-#f = urllib2.urlopen(songlink)
-#with open(filename, "wb") as code:
-#   code.write(f.read())
-#
-## POST with form-encoded data
-#r = requests.post(url, data=payload)
-#print r.text
-#
-## POST with JSON
-#import json
-#r = requests.post(url, data=json.dumps(payload))
-#print r.text
-#
-## Response, status etc
-#r.text
-#r.status_code
+    r = requests.get(url, params=payload)
+    contents = r.text
+    d = json.loads(contents, encoding="utf-8")
+    if(d.has_key('data')!=True ):#or d.has_key('song')!=True or d.has_key('songid')!=True
+        continue
+    songid = d["data"]["song"][0]["songid"]
+    print "find songid: "
+    print songid
+
+    url = "http://music.baidu.com/data/music/fmlink"
+    payload = {'songIds': songid, 'type': 'flac'}
+    r = requests.get(url, params=payload)
+    #print r.text
+    contents = r.text
+    d = json.loads(contents, encoding="utf-8")
+    #print d
+    #print json_str
+    if(d.has_key('data')!=True | d.has_key('songList')!=True | d.has_key('songLink')!=True):
+        continue
+    songlink = d["data"]["songList"][0]["songLink"]
+    print "find songlink: "
+    if(len(songlink) < 10):
+        print "do not have flac\n"
+        continue
+    print songlink
+
+    songdir = "songs_dir"
+    if not os.path.exists(songdir):
+        os.makedirs(songdir)
+
+    songname = d["data"]["songList"][0]["songName"]
+    artistName = d["data"]["songList"][0]["artistName"]
+    filename = "./" + songdir + "/"+songname+"-"+artistName+".flac"
+    print filename + " is downing now ......\n\n"
+
+    f = urllib2.urlopen(songlink)
+    with open(filename, "wb") as code:
+        code.write(f.read())
