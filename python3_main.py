3f3e5fa1e45abb55eeeef4c7c99a1dcee35910db
YongHao Hu <christopherwuy@gmail.com>
2016-04-30 11:08:16 +0800

    Python3_main.py: Add python3 implementation.

---
+++ python3_main.py
@@ -0,0 +1,63 @@
+import re
+import requests
+import json
+import urllib.request
+import urllib.error
+import os
+import sys
+
+print ("fetching msg from %s \n" %sys.argv[1])
+url = re.sub("#/", "", sys.argv[1])
+r   = requests.get(url)
+contents = r.text
+res = r'<ul class="f-hide">(.*?)</ul>'
+mm  =  re.findall(res, contents, re.S|re.M)
+CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
+if(mm):
+    contents = mm[0]
+else:
+    print ('Can not fetch information form URL. Please make sure the URL is right.\n')
+    os._exit(0)
+
+res = r'<li><a .*?>(.*?)</a></li>'
+mm  =  re.findall(res, contents, re.S|re.M)
+
+for value in mm:
+    url = 'http://sug.music.baidu.com/info/suggestion'
+    payload = {'word': value, 'version': '2', 'from': '0'}
+    print (value)
+
+    r = requests.get(url, params=payload)
+    contents = r.text
+    d = json.loads(contents, encoding="utf-8")
+    if d is not None and 'data' not in d:
+        continue
+    songid = d["data"]["song"][0]["songid"]
+    print ("find songid: %s" %songid)
+
+    url = "http://music.baidu.com/data/music/fmlink"
+    payload = {'songIds': songid, 'type': 'flac'}
+    r = requests.get(url, params=payload)
+    contents = r.text
+    d = json.loads(contents, encoding="utf-8")
+    if('data' not in d):
+        continue
+    songlink = d["data"]["songList"][0]["songLink"]
+    print ("find songlink: ")
+    if(len(songlink) < 10):
+        print ("\tdo not have flac\n")
+        continue
+    print (songlink)
+
+    songdir = "songs_dir"
+    if not os.path.exists(songdir):
+        os.makedirs(songdir)
+
+    songname = d["data"]["songList"][0]["songName"]
+    artistName = d["data"]["songList"][0]["artistName"]
+    filename = ("%s/%s/%s-%s.flac" % (CURRENT_PATH, songdir, songname, artistName))
+    print ("%s is downloading now ......\n\n" %filename)
+
+    f = urllib.request.urlopen(songlink)
+    with open(filename, "wb") as code:
+        code.write(f.read())
