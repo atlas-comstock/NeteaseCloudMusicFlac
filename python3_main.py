460fd2bf972afa1aecf5eed6979f353c9fea9bcb
wslshablin <wslshanlin@sina.com>
2018-08-05 21:41:03 +0800

    新增日志输出

--- python3_main.py
+++ python3_main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
+
+import logging
 import requests
 import json
 import urllib.request
@@ -8,8 +10,34 @@ import os
 import sys
 import unicodedata
 
-minimumsize = 10
-print("fetching msg from %s \n" % sys.argv[1])
+MINIMUM_SIZE = 10
+LOG_LEVEL = logging.INFO
+LOG_FILE = 'download.log' or False
+LOG_FORMAT = '%(asctime)s %(filename)s:%(lineno)d [%(levelname)s] %(message)s'
+
+
+def set_logger():
+    logger = logging.getLogger()
+    logger.setLevel(LOG_LEVEL)
+    formatter = logging.Formatter(fmt=LOG_FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    if LOG_FILE:
+        fh = logging.FileHandler(LOG_FILE)
+        fh.setLevel(LOG_LEVEL)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
+
+
+logger = set_logger()
+
+logger.info("fetching msg from %s \n" % sys.argv[1])
 url = re.sub("#/", "", sys.argv[1]).strip()
 r = requests.get(url, headers={
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'})
@@ -20,7 +48,7 @@ CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 if (mm):
     contents = mm[0]
 else:
-    print('Can not fetch information form URL. Please make sure the URL is right.\n')
+    logger.error('Can not fetch information form URL. Please make sure the URL is right.\n')
     os._exit(0)
 
 res = r'<li><a .*?>(.*?)</a></li>'
@@ -30,7 +58,7 @@ for value in mm:
     url = 'http://sug.music.baidu.com/info/suggestion'
     payload = {'word': value, 'version': '2', 'from': '0'}
     value = value.replace('\\xa0', ' ')  # windows cmd 的编码问题
-    print(value)
+    logger.info(value)
 
     r = requests.get(url, params=payload)
     contents = r.text
@@ -38,7 +66,7 @@ for value in mm:
     if d is not None and 'data' not in d:
         continue
     songid = d["data"]["song"][0]["songid"]
-    print("find songid: %s" % songid)
+    logger.info("find songid: %s" % songid)
 
     url = "http://music.baidu.com/data/music/fmlink"
     payload = {'songIds': songid, 'type': 'flac'}
@@ -48,11 +76,11 @@ for value in mm:
     if ('data' not in d) or d['data'] == '':
         continue
     songlink = d["data"]["songList"][0]["songLink"]
-    print("find songlink: ")
+    logger.info("find songlink: ")
     if (len(songlink) < 10):
-        print("\tdo not have flac\n")
+        logger.warning("\tdo not have flac\n")
         continue
-    print(songlink)
+    logger.info(songlink)
 
     songdir = "songs_dir"
     if not os.path.exists(songdir):
@@ -79,16 +107,16 @@ for value in mm:
         continue
 
     # Download unfinished Flacs again.
-    if not os.path.isfile(filename) or os.path.getsize(filename) < minimumsize:  # Delete useless flacs
-        print("%s is downloading now ......\n\n" % songname)
-        if size >= minimumsize:
+    if not os.path.isfile(filename) or os.path.getsize(filename) < MINIMUM_SIZE:  # Delete useless flacs
+        logger.info("%s is downloading now ......\n\n" % songname)
+        if size >= MINIMUM_SIZE:
             with open(filename, "wb") as code:
                 code.write(f.read())
         else:
-            print("the size of %s (%r Mb) is less than 10 Mb, skipping" %
+            logger.warning("the size of %s (%r Mb) is less than 10 Mb, skipping" %
                   (filename, size))
     else:
-        print("%s is already downloaded. Finding next song...\n\n" % songname)
+        logger.info("%s is already downloaded. Finding next song...\n\n" % songname)
 
-print("\n================================================================\n")
-print("Download finish!\nSongs' directory is %s/songs_dir" % os.getcwd())
+logger.info("\n================================================================\n")
+logger.info("Download finish!\nSongs' directory is %s/songs_dir" % os.getcwd())
