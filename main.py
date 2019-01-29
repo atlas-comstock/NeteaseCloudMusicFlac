091a41e6b1d22b2788e3e2a75eb8ef8a5b6719f6
moenn <dance.jms@gmail.com>
2019-01-29 20:02:55 +0800

    修改log信息

--- main.py
+++ main.py
@@ -26,9 +26,7 @@ HEADERS = {
 }
 
 
-def init():
-    if not os.path.exists(DOWNLOAD_DIR):
-        os.mkdir(DOWNLOAD_DIR)
+
 
 
 def get_args():
@@ -72,12 +70,12 @@ def fetch_song_list(url):
     contents = r.text
     song_list_name = re.search(r"<title>(.+)</title>", contents).group(1)[:-13]
 
-    logger.info("fetching Netease song list from " + song_list_name + "\n")
+    logger.info("歌单: " + song_list_name + "\n")
     pattern = r'<li><a href="/song\?id=\d+">(.+?)</a></li>'
     song_list = re.findall(pattern, contents)
     if not song_list:
         logger.error(
-            'Can not fetch information form URL. Please make sure the URL is right.\n')
+            '不能解析歌单 url\n')
         sys.exit(1)
 
     return song_list_name, song_list
@@ -97,17 +95,18 @@ def get_songid(value):
     BAIDU_SUGGESTION_API = 'http://sug.music.baidu.com/info/suggestion'
     payload = {'word': value, 'version': '2', 'from': '0'}
     value = value.replace('\\xa0', ' ')  # windows cmd 的编码问题
-    logger.info(value)
+    
 
     r = requests.get(BAIDU_SUGGESTION_API, params=payload, headers=HEADERS)
     contents = r.text
     d = json.loads(contents, encoding="utf-8")
     if not d or "errno" in d:
+        logger.info("未查找到歌曲 %s 对应的ID" % value)
         return ""
     else:
         songid = d["data"]["song"][0]["songid"]
-        logger.info("Find songid: %s" % songid)
-    return songid
+        logger.info("歌曲 %s 对应的ID为: %s" % (value, songid))
+        return songid
 
 
 def get_song_info(songid):
@@ -135,13 +134,15 @@ def get_song_info(songid):
     else:
         song_info['data'] = False
 
-    logger.info("Get song info %s" % json.dumps(song_info))
+    logger.info("获取歌曲信息 %s" % json.dumps(song_info))
     return song_info
 
 
 def download_song(song_info, mp3_option, download_folder):
     if(song_info['data']):
         if not mp3_option and song_info['size'] < 10:
+            logger.info("%s-%s 文件大小小于 10MB, 放弃下载。" %
+                        (song_info['songname'], song_info['artist']))
             return None
         else:
             filename = "{0}-{1}.flac".format(
@@ -150,23 +151,26 @@ def download_song(song_info, mp3_option, download_folder):
 
             filepath = os.path.join(download_folder, filename)
             r = requests.get(song_info['link'], headers=HEADERS, timeout=4)
-            logger.info("Downloading : %s" % filepath)
+            logger.info("下载中: %s" % filepath)
             with open(filepath, 'wb') as f:
                 for chunk in r.iter_content(chunk_size=1024):
                     if chunk:
                         f.write(chunk)
 
-            logger.info("%s downloaded" % filepath)
+            logger.info("下载完成: %s " % filepath)
 
 
 def main():
     start = time.time()
 
-    init()
+    if not os.path.exists(DOWNLOAD_DIR):
+        os.mkdir(DOWNLOAD_DIR)
+
     url, mp3_option = get_args()
     if mp3_option:
         logger.info("将下载所有歌曲, 包括 MP3 格式.")
     song_list_name, song_list = fetch_song_list(url)
+    logger.info("歌单中包含的歌曲有: %s" % song_list)
 
     pool = Pool()
     song_ids = pool.map(get_songid, song_list)
@@ -176,10 +180,10 @@ def main():
     if not os.path.exists(download_folder):
         os.mkdir(download_folder)
 
-    logger.info("Start Downloading")
+    logger.info("获取歌曲信息完成，开始下载。")
     download = partial(download_song, mp3_option=mp3_option,
                        download_folder=download_folder)
-    
+
     for i in range(1, len(song_infos), 4):
         pool.map(download, song_infos[i-1:i+4-1])
         time.sleep(1)
@@ -187,9 +191,10 @@ def main():
     pool.close()
     pool.join()
     end = time.time()
-    logger.info("cost %s s", str(end - start))
+    logger.info("共耗时 %s s", str(end - start))
 
 
+# 禁止 requests 模组使用系统代理
 os.environ['no_proxy'] = '*'
 logger = set_logger()
 
